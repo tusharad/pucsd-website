2e3c314ae6a14f39860fcbf04c36b3c93cdd3ca8
tusharad <tusharadhatrao@gmail.com>
2021-09-19 15:17:21 +0530

    Removing Featured post

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Header from "./Components/Header";
 import MainFeaturedPost from "./Components/MainFeaturedPost";
-import FeaturedPost from "./Components/FeaturedPost";
 import Main from "./Components/Main";
 import Sidebar from "./Components/Sidebar";
 import Footer from "./Components/Footer";
@@ -63,41 +62,16 @@ const mainFeaturedPost = {
   linkText: "Continue reading…",
 };
 
-const featuredPosts = [
-  {
-    title: "Featured post",
-    date: "Nov 12",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    image: "https://source.unsplash.com/random",
-    imageLabel: "Image Text",
-  },
-  {
-    title: "Post title",
-    date: "Nov 11",
-    description:
-      "This is a wider card with supporting text below as a natural lead-in to additional content.",
-    image: "https://source.unsplash.com/random",
-    imageLabel: "Image Text",
-  },
-];
-
 const sidebar = {
   title: "About",
   description:
     "Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.",
   archives: [
-    { title: "March 2020", url: "#" },
-    { title: "February 2020", url: "#" },
-    { title: "January 2020", url: "#" },
-    { title: "November 1999", url: "#" },
-    { title: "October 1999", url: "#" },
-    { title: "September 1999", url: "#" },
-    { title: "August 1999", url: "#" },
-    { title: "July 1999", url: "#" },
-    { title: "June 1999", url: "#" },
-    { title: "May 1999", url: "#" },
-    { title: "April 1999", url: "#" },
+    {
+      title: "Advertisement: Assistant Professor (Contractual July 2021)",
+      url: "#",
+    },
+    { title: "MSc Entrance Exam Syllabus 2021", url: "#" },
   ],
   social: [
     { name: "GitHub", icon: GitHubIcon },
@@ -116,11 +90,6 @@ export default function App() {
         <Header title="PUCSD" sections={sections} />
         <main>
           <MainFeaturedPost post={mainFeaturedPost} />
-          <Grid container spacing={4}>
-            {featuredPosts.map((post) => (
-              <FeaturedPost key={post.title} post={post} />
-            ))}
-          </Grid>
           <Grid container spacing={5} sx={{ mt: 3 }}>
             <Main title="From the firehose" />
             <Sidebar
--- src/Components/MenuItems.js
+++ src/Components/MenuItems.js
@@ -1,9 +1,13 @@
+import Divider from "@mui/material/Divider";
 const MenuItems = ({ data }) => {
   const items = data.split(",");
   return (
     <>
       {items.map((item, key) => (
-        <p key={item}>{item}</p>
+        <>
+          <p key={item}>{item}</p>
+          <Divider />
+        </>
       ))}
     </>
   );
