337aa4e3db91582d253c3c50258668419ed6658c
Tushar Adhatrao <tusharadhatrao@gmail.com>
2022-03-15 22:40:54 +0530

    Ant design

---
+++ pucsd-frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { DatePicker } from 'antd';
+import MainPage from './components/MainPage';
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
