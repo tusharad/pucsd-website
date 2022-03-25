2ba31eaa8d15ccc353626769691b83a361519d37
Tushar Adhatrao <tusharadhatrao@gmail.com>
2022-03-25 19:00:04 +0530

    First commit

--- pucsd-frontend/src/App.js
+++ pucsd-frontend/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
-import { DatePicker } from 'antd';
-import MainPage from './components/MainPage';
+import React from 'react';
+import {
+  ChakraProvider,
+} from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <ChakraProvider>
+      <Box >
+        <Header />
+        <Main />
+        <Footer />
+      </Box>
+    </ChakraProvider>
   );
 }
 
---
+++ pucsd-frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () =>{
+    return(
+        <div>
+        <h1>Welcome to Department of Computer Science, SP Pune University</h1>
+        </div>
+    )
+}
+
+export default Header;
